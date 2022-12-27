b51f845be5e50d5d2a3731a0f0427e480f0fc893
Lucas Tiago de Moraes <lucastiagodemoraes@gmail.com>
2022-12-27 19:22:46 -0300

    Added code to work with path level and method any

--- src/route.ts
+++ src/route.ts
@@ -1,4 +1,4 @@
-import type {MojoApp} from '@mojojs/core';
+import type {MojoApp, MojoRoute} from '@mojojs/core';
 import {existsSync, lstatSync} from 'node:fs';
 import globSync from 'glob';
 
@@ -7,62 +7,108 @@ export default function routePlugin(app: MojoApp, options: {namespace?: string} 
 }
 
 class Route {
+  private app: MojoApp;
+  private router: MojoRoute;
   private routes: any;
 
   constructor(app: MojoApp, options: {namespace?: string} = {}) {
+    this.app = app;
+    this.router = app.router;
     this.routes = {};
 
+    // check if the namespace is setted, default is 'route'
     const namespace = options.namespace ?? 'route';
 
-    const tsPathRoutes = app.home + '/lib/' + namespace; 
+    // it is path using typescript
+    const tsPathRoutes = app.home + '/lib/' + namespace;
+    
+    // it is path using javascript
     const jsPathRoutes = app.home + '/' + namespace;
 
     let pathRoutes = '';
     
+    // check if exists path when it is using typescript
     if (existsSync(tsPathRoutes))
       pathRoutes = tsPathRoutes;
 
+    // check if exists path when it is using javascript (default)
     if (existsSync(jsPathRoutes))
       pathRoutes = jsPathRoutes;      
     
     if (!existsSync(pathRoutes)) 
       throw Error(`Routes path ${pathRoutes} does not exist!`); 
       
-    this.files(app, namespace, pathRoutes);
+    this.files(namespace, pathRoutes);
   }
 
-  private async files(app: MojoApp, namespace: string, pathRoutes: string) {
+  private async files(namespace: string, pathRoutes: string) {
     globSync(pathRoutes + '/**/*.js', {mark: true}, (er, files) => {
-      files.forEach(file => {
+      files.forEach(async file => {
         if (lstatSync(file.toString()).isFile()) {
-          this.loadFile(app, namespace, file);
+          await this.loadFile(namespace, file);
         }
       });
-    });    
+    });
   }
   
-  private async loadFile(app: MojoApp, namespace: string, file: string) {
+  private async loadFile(namespace: string, file: string) {
     const imports = await import(file.toString());
 
+    // get name to reference
     const re = new RegExp(`/${namespace}/([^\.]+)\.js$`);
     const match = file.match(re);
     const name = match ? match[1] : '';
 
-    const under = imports['under'];
-    if (under !== undefined) {
-      let run = await under(app);
+    // get base to reference
+    const bases = name.split('/');
+    bases.pop();
+    const base = bases.join('/');
+
+    // get app to check if need set app in the methods any, under and route
+    const app = imports['app'];
+
+    // check if exist method 'any', if is true load the method
+    const any = imports['any'];
+    if (any !== undefined) {
+      let run;
+      
+      if (app)
+        run = await any(this.app, this.router, this.routes[base]);
+      else
+        run = await any(this.router, this.routes[base]);
 
       if (run !== undefined)
-        this.routes[name] = run;
+      this.routes[name] = run;
+    }    
+
+    // check if exist method 'under', if is true load the method
+    const under = imports['under'];
+    if (under !== undefined) {
+      let run;
+      
+      if (app)
+        run = await under(this.app, this.router, this.routes[base]);
+      else
+        run = await under(this.router, this.routes[base]);
+
+      if (run !== undefined)
+      this.routes[name] = run;
     }
 
+    // check if exist method 'route', if is true load the method
     const route = imports['route'];
     if (route !== undefined) {
-      if (match && this.routes[match[1]]) {
-        route(app, this.routes[match[1]], this.routes[name] ?? undefined);     
+      if (this.routes[base]) {
+        if (app)
+          await route(this.app, this.router, this.routes[base], this.routes[name]);     
+        else
+          await route(this.router, this.routes[base], this.routes[name]);     
       } else { 
-        route(app, this.routes[name] ?? undefined);    
+        if (app)
+          await route(this.app, this.router, this.routes[name]);  
+        else
+          await route(this.router, this.routes[name]);  
       }
-    }    
+    }
   }
 }
--- test/test.js
+++ test/test.js
@@ -4,16 +4,31 @@ import t from 'tap';
 
 t.test('routePlugin', async t => {
   const app = mojo({mode: 'testing'});
-  app.plugin(routePlugin);
+  await app.plugin(routePlugin);
 
   const ua = await app.newTestUserAgent({tap: t});
+
+  await ua.get('/page');
   
-  await t.test('Route Foo', async () => {
-    (await ua.getOk('/foo')).statusIs(200).bodyLike(/Foo/);
+  await t.test('Route Page', async () => {    
+    (await ua.getOk('/page')).statusIs(200).bodyLike(/Page/);
   });
 
-  await t.test('Route Foo Bar', async () => {
-    (await ua.getOk('/foo/bar')).statusIs(200).bodyLike(/Foo Bar/);
+  await t.test('Route Page Bar', async () => {
+    (await ua.getOk('/page/bar')).statusIs(200).bodyLike(/Page Bar/);
+  });
+
+  await t.test('Route Page Baz', async () => {
+    (await ua.getOk('/page/baz')).statusIs(200).bodyLike(/Page Baz/);
+  });   
+
+  await t.test('Route Page Foo', async () => {
+    (await ua.getOk('/page/foo')).statusIs(200).bodyLike(/Page Foo/);
+  });  
+
+  await t.test('Route Account Sign-In', async () => {
+    (await ua.postOk('/account/sign-in', {form: {user: 'foo', pass: 'bar'}})).statusIs(200).bodyLike(/True/);
+    (await ua.postOk('/account/sign-in', {form: {user: 'foo', pass: 'baz'}})).statusIs(200).bodyLike(/False/);
   });  
 
   await ua.stop();
